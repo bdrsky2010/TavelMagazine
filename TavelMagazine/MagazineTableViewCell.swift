cc586852f315ffd9f70278bb7644ab04255c7032
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-27 00:32:55 +0900

    UI 구성 및 에러
    킹피셔를 이미지 로드 에러
    - 금일 해결 예정

--- TavelMagazine/MagazineTableViewCell.swift
+++ TavelMagazine/MagazineTableViewCell.swift
@@ -11,9 +11,29 @@ class MagazineTableViewCell: UITableViewCell {
 
     static let reuseIdentifier =  "MagazineTableViewCell"
     
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var infoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        selectionStyle = .none
+        configUI()
+    }
+    
+    private func configUI() {
+        infoImageView.layer.cornerRadius = 10
+        
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        subtitleLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        subtitleLabel.textColor = .systemGray
+        
+        dateLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        dateLabel.textColor = .systemGray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- TavelMagazine/MagazineTableViewController.swift
+++ TavelMagazine/MagazineTableViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Kingfisher
+//import KingfisherWebP
 
 class MagazineTableViewController: UITableViewController {
 
@@ -14,5 +16,39 @@ class MagazineTableViewController: UITableViewController {
 
         navigationItem.title = "떠나보자 TRAVEL"
         
+        tableView.rowHeight = 450
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Magazine.magazineList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = MagazineTableViewCell.reuseIdentifier
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! MagazineTableViewCell
+        
+        let index = indexPath.row
+        let magazine = Magazine.magazineList[index]
+        
+        let url = URL(string: magazine.photo_image)
+        
+        cell.infoImageView.kf.indicatorType = .activity
+        cell.infoImageView.kf.setImage(with: url) { result in
+            switch result {
+            case .success(let value):
+                print(value.image)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+//        cell.infoImageView.kf.setImage(with: url, options: [.processor(WebPProcessor.default), .cacheSerializer(WebPSerializer.default)])
+        
+        
+        cell.titleLabel.text = magazine.title
+        cell.subtitleLabel.text = magazine.subtitle
+        cell.dateLabel.text = magazine.date.convertStrDate
+        
+        return cell
     }
 }
