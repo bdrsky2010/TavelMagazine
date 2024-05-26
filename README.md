cf010894e5256e7cad53db50ea8cb58265bc254b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-26 22:25:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TavelMagazine
+ 여행 매거진 미니 프로젝트
---
+++ TavelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TavelMagazine
+//
+//  Created by Minjae Kim on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
