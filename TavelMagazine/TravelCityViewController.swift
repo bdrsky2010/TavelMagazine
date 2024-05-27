e35b5275b45b5dfc6977a129e8ecc8c543d485a2
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-27 20:20:36 +0900

    TabBar 파일 분리

---
+++ TavelMagazine/TravelCityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelCityViewController.swift
+//  TavelMagazine
+//
+//  Created by Minjae Kim on 5/27/24.
+//
+
+import UIKit
+
+class TravelCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
