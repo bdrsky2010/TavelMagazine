db76e3ce9e00e370219619562287fd87f15eb6ab
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-27 19:46:15 +0900

    TableViewController -> ViewController, TableView, TableViewCell
    - Magazine
       - TableView, TableViewCell 오토레이아웃 셋팅 완료
       - Outlet 변수 연결 완료

---
+++ TavelMagazine/MagazineViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MagazineViewController.swift
+//  TavelMagazine
+//
+//  Created by Minjae Kim on 5/27/24.
+//
+
+import UIKit
+
+class MagazineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
