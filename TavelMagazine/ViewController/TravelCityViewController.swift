38a8840a5b398adf854aa0a9f9fbafd9741cffae
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-05-27 20:27:28 +0900

    그룹화 진행
    - Storyboard
    - Model
    - ViewController
    - TableViewCell

---
+++ TavelMagazine/ViewController/TravelCityViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelCityViewController.swift
+//  TavelMagazine
+//
+//  Created by Minjae Kim on 5/27/24.
+//
+
+import UIKit
+
+class TravelCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
